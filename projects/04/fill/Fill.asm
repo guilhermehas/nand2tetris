9cb7efa904550cd4622f633bc6f6c0010d671727
Guilherme <guilhermehas@hotmail.com>
2018-12-27 20:40:03 -0200

    solved project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,62 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@LASTKEY
+M=0
+(START)
+@SCREEN
+D=A
+@addr
+M=D // addr = 16384
+
+@8192
+D=A
+@n
+M=D // n = NMAX
+@i
+M=0 // i = 0
+
+(LOOP)
+@i
+D=M
+@n
+D=D-M
+@END
+D;JGT // if i>n goto END
+@LASTKEY
+D=M
+
+@addr
+A=M
+M=D // RAM[addr]=1111111111111111
+
+@i
+M=M+1 // i = i + 1
+@32
+D=A
+@addr
+M=M+1 // addr = addr + 32
+@LOOP
+0;JMP // goto LOOP
+
+(CHANGENEG)
+@1111111111111111
+D=A
+@LASTKEY
+M=D
+@START
+0;JMP
+
+(END)
+@KBD
+D=M
+@CHANGENEG
+D;JNE
+
+@0
+D=A
+@LASTKEY
+M=D
+@START
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@i
+M=0
+
+@R2 // Storing in RAM[2]
+M=0
+
+(LOOP)
+@i
+D=M
+@R1
+D=D-M
+@END
+D;JEQ
+
+@R0
+D=M
+@R2
+M=D+M
+@i
+M=M+1
+@LOOP
+0;JMP
+
+(END)
+@END
+0; JMP
